eb2726928c38c1a3c021b8f96e81f5d161ba466a
даня <@DESKTOP-AEISPNI>
2022-12-02 13:34:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace kosti
+
+{
+
+    public class Class1
+
+    {
+        public int p;
+        public int pr
+        {
+            get { return p; }
+            set { p = value; }
+        }
+        public int t;
+
+        public int tr
+        {
+            get { return t; }
+            set { t = value; }
+        }
+        public int randomNumbers()
+        {
+            Random r = new Random();
+            int sum = r.Next(1, 7);
+            return sum;
+        }
+        public int increasePoints(int l)
+        {
+            l += randomNumbers();
+            return l;
+        }
+        public int numberThrows()
+        {
+            t++;
+            return t;
+        }
+    }
+}
